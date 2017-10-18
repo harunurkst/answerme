68f7eb62f4bb4505985eb5e96637585de5794531
Rabiul Islam <rb60041@gmail.com>
2017-10-18 23:35:05 +0600

    added question form and answer form to home page

--- question/views.py
+++ question/views.py
@@ -8,10 +8,11 @@ from answer.forms import AnswerForm
 
 
 def index(request):
+    # all question list for displaying on home page
     # retrieve question queryset
     questions = Question.objects.filter(is_banned=False)
 
-    #add paginator
+    # adding paginator
     page = request.GET.get('page', 1)
     paginator = Paginator(questions, 10) # 10 question per page
     try:
@@ -21,30 +22,15 @@ def index(request):
     except EmptyPage:
         questions = paginator.page(paginator.num_pages)
 
-    #adding page object to context
-    context = {'questions': questions}
+    # initialize form objects for question and answer
+    question_form = QuestionForm()
+    answer_form = AnswerForm()
+
+    context = {'questions': questions,
+               'question_form':question_form,
+               'answer_form': answer_form}
+
     return render(request, 'question/index.html', context)
-
-def add_question(request):
-    context = {}
-    if request.method == 'POST':
-        form = QuestionForm(request.POST) # form instance for post request
-        context['form']= form
-
-        if form.is_valid():
-            qs_text = form.cleaned_data.get('text')
-            # creating new question on database
-            q = Question()
-            q.text = qs_text
-            if request.user.is_authenticated():
-                q.user = request.user
-            q.save()
-            return redirect('/')
-    else:
-        form = QuestionForm() # form instance for get request
-        context['form']=form
-
-    return render(request, 'question/add_question.html', {'form':form})
 
 
 def question_detail(request, pk):
@@ -78,3 +64,24 @@ def question_detail(request, pk):
 
     return render(request, 'question/detail.html', context)
 
+
+def add_question(request):
+    context = {}
+    if request.method == 'POST':
+        question_form = QuestionForm(request.POST) # form instance for post request
+        context['form']= question_form
+
+        if question_form.is_valid():
+            qs_text = question_form.cleaned_data.get('text')
+            # creating new question on database
+            qs = Question()
+            qs.text = qs_text
+            if request.user.is_authenticated():
+                qs.user = request.user
+            qs.save()
+            return redirect('question:detail', pk=qs.id)
+    else:
+        question_form = QuestionForm() # form instance for get request
+        context['form']=question_form
+
+    return render(request, 'question/add_question.html', {'form':question_form})
