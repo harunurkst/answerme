3beafa2de37cf667e3cb4d1ea5c797fcd39d6767
rabiulislam993 <rb60041@gmail.com>
2017-11-05 18:38:02 +0600

    marged with tuhin vai's code

--- accounts/models.py
+++ accounts/models.py
@@ -23,17 +23,17 @@ class Profile(models.Model):
         
     @receiver(post_save, sender=SocialAccount)
     def social_login_profilepic(instance, **kwargs):
-    preferred_profile_size_pixels = 256
-    request_user_id =instance.user_id
-    user_intance = User.objects.get(id=request_user_id)
-    if instance.provider == 'facebook':
-        account_uid = SocialAccount.objects.filter(user_id=request_user_id, provider='facebook')
-        UID = account_uid[0].extra_data['id']
-        object, create = Profile.objects.get_or_create(user_id=request_user_id)
-        picture_url = "http://graph.facebook.com/{0}/picture?width={1}&height={1}".format(
-                    UID, preferred_profile_size_pixels)
-        if create:
-                object.user = user_intance
-                object.profile_pic_url = picture_url
-                object.save()
+        preferred_profile_size_pixels = 256
+        request_user_id =instance.user_id
+        user_intance = User.objects.get(id=request_user_id)
+        if instance.provider == 'facebook':
+            account_uid = SocialAccount.objects.filter(user_id=request_user_id, provider='facebook')
+            UID = account_uid[0].extra_data['id']
+            object, create = Profile.objects.get_or_create(user_id=request_user_id)
+            picture_url = "http://graph.facebook.com/{0}/picture?width={1}&height={1}".format(
+                        UID, preferred_profile_size_pixels)
+            if create:
+                    object.user = user_intance
+                    object.profile_pic_url = picture_url
+                    object.save()
 
