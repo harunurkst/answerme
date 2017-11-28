d3350808ed88a8cbf1776fee5b30976b6b0e707e
rabiulislam993 <rb60041@gmail.com>
2017-11-29 00:06:47 +0600

    question form converted to TextArea

--- question/forms.py
+++ question/forms.py
@@ -3,13 +3,8 @@ from django import forms
 
 class QuestionForm(forms.Form):
     text = forms.CharField(label='',
-                           widget=forms.TextInput(attrs={'placeholder': 'Write what you want to know',
-                                                         'class': 'ask-input w-input',
-                                                         'maxlength':'256',
-                                                         'autofocus':'true',
-                                                         'name':'name',
-                                                         'data':'Name',
-                                                         'id':'name',
+                           widget=forms.Textarea(attrs={'placeholder': 'Write what you want to know',
+                                                         'class': 'form-control',
                                                          }))
 
     class Meta:
